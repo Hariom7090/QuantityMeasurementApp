d0e028e4224e3556a80b2216417e8b6387fa864f
Hariom7090 <hariom7090.ca22@chitkara.edu.in>
2026-04-17 10:09:56 +0530

    add refactorUnitClass

--- measurement/src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ measurement/src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -1,29 +1,29 @@
 package com.quantity.measurement.enums;
 
-
-
 public enum LengthUnit {
 
-    FEET(12.0),
-
-    INCH(1.0),
-
-    YARDS(36.0),
-
-    CENTIMETERS(0.393701);
+    FEET(1.0),
+    INCH(1.0 / 12),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
 
     private final double toFeetFactor;
 
-    LengthUnit(double toInchFactor) {
-        this.toFeetFactor = toInchFactor;
+    LengthUnit(double toFeetFactor) {
+        this.toFeetFactor = toFeetFactor;
     }
 
-    public double toFeet(double value) {
+    public double convertToBaseUnit(double value) {
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid value");
+        }
         return value * toFeetFactor;
     }
 
-    public double fromFeet(double value){
+    public double convertFromBaseUnit(double value) {
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid value");
+        }
         return value / toFeetFactor;
-
     }
 }
